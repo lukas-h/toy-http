585d9d425bc881eb6c9ad27d583ef4f756e27ecc
lukas himsel <lukas.himsel@web.de>
2015-12-26 16:05:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Copyright (C) 2015 Lukas Himsel; Licensed under GNU GPL v2 (or later)
-gcc -c toy-http.c
-gcc -c service.c
-gcc -o toy-http toy-http.o service.o
+cc -c toy-http.c
+cc -c service.c
+cc -o toy-http toy-http.o service.o
 
 rm service.o
 rm toy-http.o
