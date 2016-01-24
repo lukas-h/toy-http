99649e17a75f9c3775f2ac143357a6f20aebf825
lukas himsel <lukas.himsel@web.de>
2016-01-24 23:13:47 +0100

    Update service.c

--- service.c
+++ service.c
@@ -4,7 +4,7 @@
  * Copyright 2015, 2016 Lukas Himsel <lukas.himsel@web.de>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
